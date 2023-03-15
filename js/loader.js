426d222399a233b6faf6c3095769004911ef0edf
Leonel-H-G <107085045+Leonel-H-G@users.noreply.github.com>
2023-03-14 18:04:13 -0600

    Delete js directory

--- js/loader.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load',()=>{
-    const contenedor_loader = document.querySelector('.contenedor_loader')
-    contenedor_loader.style.opacity = 0
-    contenedor_loader.style.visibility = 'hidden'
-})
