fa16024e1f5d2322f02833a7b3418124caab7ca7
IscJlchavezG <46545953+JlchavezG@users.noreply.github.com>
2023-03-21 10:38:23 -0600

    Add files via upload

---
+++ VFAST/css/loader.js
@@ -0,0 +1,5 @@
+window.addEventListener('load',()=>{
+    const contenedor_loader = document.querySelector('.contenedor_loader')
+    contenedor_loader.style.opacity = 0
+    contenedor_loader.style.visibility = 'hidden'
+})
