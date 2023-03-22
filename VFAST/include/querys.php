85d95c922ecef2cb00d278b9b1ab098237e41a37
Leonel-H-G <107085045+Leonel-H-G@users.noreply.github.com>
2023-03-21 22:59:29 -0600

    Delete VFAST directory

--- VFAST/include/querys.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include 'connect.php';
-    $tusuario = "SELECT * FROM usuarios ORDER BY Nombre";
-    $tusuarioej = $conexion->query($tusuario);
-
-    //$cortar = $tusuarioej->fetch_array();
-?>
