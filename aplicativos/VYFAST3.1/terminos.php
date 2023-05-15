89ba5e4582b98520b87a420122f69b8dc87406b5
MoY-m <107066626+MoY-m@users.noreply.github.com>
2023-05-14 21:15:22 -0600

    Add files via upload

---
+++ aplicativos/VYFAST3.1/terminos.php
@@ -0,0 +1,27 @@
+
+<!-- Modal -->
+<div class="modal fade" id="terminos" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Aviso de Privacidad</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      V.Fast conforme a lo establecido a la Ley Federal de Protección de Datos Personales en Posesión de los Particulares de Los Estados Unidos Mexicanos ponemos a disposición de nuestros clientes, el presente aviso de privacidad.
+El objetivo de esta Política de Privacidad es informarte sobre qué datos recogemos, por qué los recogemos y cómo puedes actualizarlos, gestionarlos, exportarlos y eliminarlos.
+Los datos personales ingresados serán utilizados únicamente para las actividades internas de la empresa.
+Los datos que solicitamos a nuestros clientes son: Nombre, Apellidos, Correo Electrónico con la finalidad poder identificar a cada usuario dentro de la página ya que se realizan acuerdos entre usuario y proveedor, sin embargo nos proporcionas información personal que incluye tu nombre y tu contraseña. También puedes añadir un número de teléfono o datos de pago a tu cuenta. 
+También se recopila la actividad dentro de la plataforma lugares registrados, viajes, interacciones, búsquedas.
+Dicha información es recopilada con el fin de ofrecer una experiencia personalizada dentro de la plataforma
+El presente aviso esta propenso a modificaciones en tal caso se notificará previamente a los usuarios.
+Recogemos información para proporcionar los mejores servicios a todos nuestros usuarios: desde determinar información básica, como el idioma que hablas, hasta datos más complejos
+Al utilizar nuestros servicios, nos confías tus datos. Entendemos que es una gran responsabilidad y nos esforzamos al máximo para proteger tu información y permitirte controlarla.
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Aceptar</button>
+      </div>
+    </div>
+  </div>
+</div>
